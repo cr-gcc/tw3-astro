84c8749505294b0ba121d4063ce450301a8cf9cc
Cris GC <cris.gcc@outlook.com>
2025-05-20 20:05:39 -0600

    Configuracion font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 import forms from '@tailwindcss/forms';
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+          raleway: ['Raleway',...defaultTheme.fontFamily.sans]
+      },
+    },
   },
   plugins: [forms],
 };
